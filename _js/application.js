8f022f09bc0c8a706bb0d39e63fba7fe500f38f6
RadamesDSantiago <radames.rex@gmail.com>
2016-01-12 09:54:59 -0300

    criação da primeira versão sem framework

--- _js/application.js
+++ _js/application.js
@@ -8,9 +8,13 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
+    
     var data = ev.dataTransfer.getData("text");  
+
     document.getElementById(data).setAttribute("draggable", "false"); 
     document.getElementById(data).setAttribute("class", "sing opaco"); 
-    ev.target.setAttribute("class", "slot borda-slot "+data);
-    document.getElementById("sound-"+data).play();
+
+    ev.target.setAttribute("class", "slot borda-slot anime-"+data);
+    ev.target.setAttribute("ondrop", "");
+    document.getElementById("sound-"+data).play();    
 }
